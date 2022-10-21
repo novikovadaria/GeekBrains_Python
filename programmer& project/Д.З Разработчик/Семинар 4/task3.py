12ea61f4ddb96ee566d0d339ed5fc3a5a840ca4a
Daria N <107907983+novikovadaria@users.noreply.github.com>
2022-10-21 14:05:01 +0300

    Add files via upload

---
+++ programmer& project/Д.З Разработчик/Семинар 4/task3.py
@@ -0,0 +1,34 @@
+# 33. Задана натуральная степень k. Сформировать случайным образом список коэффициентов
+# (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
+#     *Пример: k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x²
+
+from random import randint
+import itertools
+
+k = randint(2, 7)
+
+
+def get_ratios(k):
+    ratios = [randint(0, 10) for i in range(k + 1)]
+    while ratios[0] == 0:
+        ratios[0] = randint(1, 10)
+    return ratios
+
+
+def get_polynomial(k, ratios):
+    var = ['*x^']*(k-1) + ['*x']
+    polynomial = [[a, b, c] for a, b, c in itertools.zip_longest(
+        ratios, var, range(k, 1, -1), fillvalue='') if a != 0]
+    for x in polynomial:
+        x.append(' + ')
+    polynomial = list(itertools.chain(*polynomial))
+    polynomial[-1] = ' = 0'
+    return "".join(map(str, polynomial)).replace(' 1*x', ' x')
+
+
+ratios = get_ratios(k)
+polynom1 = get_polynomial(k, ratios)
+print(polynom1)
+
+with open('33_Polynomial.txt', 'w') as data:
+    data.write(polynom1)
